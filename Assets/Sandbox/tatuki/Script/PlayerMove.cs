d348ff36d96be39813aa4bd55c49ffac1e4886ed
ohasi084 <92529768+ohasi084@users.noreply.github.com>
2023-10-15 17:02:29 +0900

    Feature/tatuki player (#19)
    * プロジェクトにInputSystemの追加
    
    * PlayerのWASD操作などの実装
    
    ---------
    
    Co-authored-by: UmataHaruto <115957986+UmataHaruto@users.noreply.github.com>

--- Assets/Sandbox/tatuki/Script/PlayerMove.cs
+++ Assets/Sandbox/tatuki/Script/PlayerMove.cs
@@ -14,19 +14,28 @@ namespace InGame.Player
         // 更新はフレームごとに1回呼び出されます
         void Update()
         {
+            Vector3 Pos;
 
+            // コントローラー操作
             // Gamepad.all で接続されているすべてのゲームパッドを列挙できる
             // TextObjects の数以上の情報は載せられないので、少ない方の数で for する
             for (int i = 0; i < Gamepad.all.Count; i++)
             {
+                Debug.Log(Gamepad.all.Count);
                 var gamepad = Gamepad.all[i];
+
+                if (Gamepad.all.Count == 1)
+                {
+                    i = 1;
+                    Debug.Log(i);
+                }
 
                 // 操作されたボタンなどの情報を取得
                 var leftStickValue = gamepad.leftStick.ReadValue();
                 var rightStickValue = gamepad.rightStick.ReadValue();
                 var dpadValue = gamepad.dpad.ReadValue();
 
-                Vector3 Pos = player[i].transform.position;
+                Pos = player[i].transform.position;
 
                 //左スティック
                 if (leftStickValue.magnitude > 0f)
@@ -45,6 +54,44 @@ namespace InGame.Player
 
                 }
             }
+
+            // キーボード操作
+            // 現在のキーボード情報
+            var current = Keyboard.current;
+
+            // キーボード接続チェック
+            if (current == null)
+            {
+                // キーボードが接続されていないと
+                // Keyboard.currentがnullになる
+                return;
+            }
+
+            Pos = player[0].transform.position;
+
+            var aKey = current.aKey;
+            var wKey = current.wKey;
+            var sKey = current.sKey;
+            var dKey = current.dKey;
+
+            if (aKey.isPressed)
+            {
+                Pos.x -= Time.deltaTime;
+            }
+            if (wKey.isPressed)
+            {
+                Pos.z += Time.deltaTime;
+            }
+            if (sKey.isPressed)
+            {
+                Pos.z -= Time.deltaTime;
+            }
+            if (dKey.isPressed)
+            {
+                Pos.x += Time.deltaTime;
+            }
+
+            player[0].transform.position = Pos;
         }
     }
 }
