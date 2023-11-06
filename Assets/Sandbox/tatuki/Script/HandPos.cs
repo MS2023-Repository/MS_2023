f1b931679d73187618266a52f2880e1acb8218d0
ohasi084 <tatukixyz1010@gmail.com>
2023-11-06 10:45:57 +0900

    playerの腕の制御（バグあり）

---
+++ Assets/Sandbox/tatuki/Script/HandPos.cs
@@ -0,0 +1,87 @@
+using InGame.Box;
+using UnityEngine;
+
+namespace InGame.Player
+{
+    public class HandPos : MonoBehaviour
+    {
+        [SerializeField] private GameObject _Player;
+        [SerializeField] private GameObject _LeftHand;
+        [SerializeField] private GameObject _RightHand;
+
+        //[SerializeField] private float _InitLeftHeight;
+        //[SerializeField] private float _InitRightHeight;
+
+        private float _LeftHeight;
+        private float _RightHeight;
+
+        [SerializeField] private GameObject _Box;
+
+        private Vector3 _LeftHandPos;
+        private Vector3 _RightHandPos;
+
+        private BoxCornerPos _BoxCornerPosScript;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _BoxCornerPosScript = _Box.GetComponent<BoxCornerPos>();
+
+            _LeftHeight = 0.0f;
+            _RightHeight = 0.0f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //_BoxCornerPosScript.SetBoxCornerPos();
+
+            ResetRotateHand();
+            ////var leftTriggerValue = gamepad.leftTrigger.ReadValue();
+            ////var rightTriggerValue = gamepad.rightTrigger.ReadValue();
+
+            ////if (leftTriggerValue > 0 || rightTriggerValue > 0)
+            ////{
+
+            ////}
+
+            //_LeftHandPos = _LeftHand.transform.position;
+            //_LeftHandPos.y = _Player.transform.position.y + _LeftHeight + _InitLeftHeight;
+            //_LeftHand.transform.position = _LeftHandPos;
+
+            //_RightHandPos = _RightHand.transform.position;
+            //_RightHandPos.y = _Player.transform.position.y + _RightHeight + _InitRightHeight;
+            //_RightHand.transform.position = _RightHandPos;
+        }
+
+        public void SetLeftHandHeight(float InitHeight,float InitWidth,float height,float width)
+        {
+            //_BoxCornerPosScript.SetBoxCornerPos();
+
+            _LeftHeight = -height * width;
+
+            _LeftHandPos = _LeftHand.transform.position;
+            _LeftHandPos = _Player.transform.position - (_Player.transform.right.normalized * InitWidth);
+            _LeftHandPos.y = _Player.transform.position.y + _LeftHeight + InitHeight;
+            _LeftHand.transform.position = _LeftHandPos;
+        }
+
+        public void SetRightHandHeight(float InitHeight,float InitWidth,float height, float range)
+        {
+            //_BoxCornerPosScript.SetBoxCornerPos();
+
+            _RightHeight = -height * range;
+
+            _RightHandPos = _RightHand.transform.position;
+            _RightHandPos = _Player.transform.position + (_Player.transform.right.normalized * InitWidth);
+            _RightHandPos.y = _Player.transform.position.y + _RightHeight + InitHeight;
+            _RightHand.transform.position = _RightHandPos;
+        }
+
+        private void ResetRotateHand()
+        {
+            _RightHand.transform.rotation = new Quaternion(0,90,0,0);
+            _LeftHand.transform.rotation = new Quaternion(0, -90, 0, 0);
+        }
+    }
+}
